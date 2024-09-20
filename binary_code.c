46f7112629283ddb8fbf021db20e3932945273ad
Aleksandra Arykova <aleksandra.arykova@alatoo.edu.kg>
2024-09-21 00:04:41 +0600

    add conditions for elseif

--- binary_code.c
+++ binary_code.c
@@ -3,8 +3,20 @@
 int main(){
     int numbersAfterPoint = 4;
     double num = 10;
-    int intValue;
+    int intValue = (int)num;
     int intPart[8];
     int decPart[numbersAfterPoint];
+    if(num - (double)intValue == 0.0 && num > 0){
+
+    }
+    else if(num - (double)intValue == 0.0 && num < 0){
+
+    }
+    else if(num - (double)intValue > 0.0 && num > 0){
+
+    }
+    else if(num - (double)intValue < 0.0 && num < 0){
+        
+    }
 
 }
