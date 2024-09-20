16d4158f8f3dbe8ac0b747e93986a5df46cd4c15
Aleksandra Arykova <aleksandra.arykova@alatoo.edu.kg>
2024-09-20 23:39:47 +0600

    first commit

---
+++ binary_code.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+    int numbersAfterPoint;
+    double num;
+    int intValue;
+    int intPart[8];
+    int decPart[numbersAfterPoint];
+
+    printf("Введите количество чисел после запятой:")
+    scanf("%d", &numbersAfterPoint);
+    printf("Введите число: ")
+    scanf("%lf", &num);
+    int intValue = (int)num;
+    if((num - intValue) == 0 && num > 0){
+        while(num != 0){
+            
+        }
+    }
+    else if()
+
+
+
+
+
+
+
+
+
+
+}
+
+void PositiveInteger(int a, ){
+
+}
