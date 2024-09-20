c702b934ca05bdd07f9e4d6139e628f074d5c3f9
Aleksandra Arykova <aleksandra.arykova@alatoo.edu.kg>
2024-09-20 23:58:08 +0600

    delete scanf

--- binary_code.c
+++ binary_code.c
@@ -1,24 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    int numbersAfterPoint;
-    double num;
+    int numbersAfterPoint = 4;
+    double num = 10;
     int intValue;
     int intPart[8];
     int decPart[numbersAfterPoint];
-
-    printf("Введите количество чисел после запятой:");
-    scanf("%d", &numbersAfterPoint);
-    printf("Введите число: ");
-    scanf("%lf", &num);
-
-
-
-
-
-
-
-
-
 
 }
