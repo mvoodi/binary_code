efc18ce596de6e2135500f0b422e920991ae1548
Aleksandra Arykova <aleksandra.arykova@alatoo.edu.kg>
2024-09-21 13:27:14 +0600

    method for positive int

--- binary_code.c
+++ binary_code.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+
 int main(){
     int numbersAfterPoint = 4;
     double num = 10;
@@ -7,6 +9,7 @@ int main(){
     int intPart[8];
     int decPart[numbersAfterPoint];
     if(num - (double)intValue == 0.0 && num > 0){
+        PositiveInt(num, intPart[]);
 
     }
     else if(num - (double)intValue == 0.0 && num < 0){
@@ -16,7 +19,20 @@ int main(){
 
     }
     else if(num - (double)intValue < 0.0 && num < 0){
-        
+
     }
 
+}
+
+static void PositiveInt(int num, int array[]){
+    int num1 = num;
+    int i = 0;
+    while(num != 0){
+        array[i] = num1 % 2;
+        num1 = num1 / 2;
+        i = i + 1;
+    }
+    for(int a = i; a >= 0; a--){
+        printf(array[a]);
+    }
 }
