83c29ba150df5eb069662a8b9c5170773cc6c253
Aleksandra Arykova <aleksandra.arykova@alatoo.edu.kg>
2024-09-22 21:08:06 +0600

    add mothod for negative int

--- binary_code.c
+++ binary_code.c
@@ -14,6 +14,43 @@ static void PositiveInt(int num, int array[]) {
     printf("\n");
 }
 
+static void NegativeInt(){
+    int num1 = num * (-1);
+    int i = 0;
+    while (num1 != 0)
+    {
+        array[i] = num1 % 2;
+        num1 = num1 / 2;
+        i = i + 1;
+    }
+
+    for(int j = 0; j < 8; j++){
+        if (array[j] == 0)
+        {
+            array[j] = 1;
+        }
+        else{
+            array[j] = 0;
+        }
+        
+    }
+    for (int j = 7; j >= 0; j--){
+        if (array[j] == 0)
+        {
+            array[j] = 1;
+            break;
+        }
+        else{
+            array[j] = 0;
+        }
+    }
+
+    for (int a = i - 1; a >= 0; a--) {
+        printf("%d ", array[a]);
+    }
+    printf("\n");
+}
+
 int main(){
     int numbersAfterPoint = 4;
     double num = 10;
@@ -24,9 +61,9 @@ int main(){
         PositiveInt(num, intPart);
 
     }
-    // else if(num - (double)intValue == 0.0 && num < 0){
+    else if(num - (double)intValue == 0.0 && num < 0){
 
-    // }
+    }
     // else if(num - (double)intValue > 0.0 && num > 0){
 
     // }
